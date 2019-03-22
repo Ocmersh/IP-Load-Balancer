75508d567e297a2674717e5732bda93a35899af3
Bryce Hansen <ocmersh@gmail.com>
2019-03-22 14:11:56 -0600

    Basic forwarding

--- PythonApplication1/monitor.py
+++ PythonApplication1/monitor.py
@@ -1,3 +1,22 @@
-#!/usr/bin/python
+from ryu.base import app_manager
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
 
-print("Hello World")
+class L2Forwarding(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+        super(L2Forwarding, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def curr_packet(self, ev):
+        msg = ev.msg
+        dpath = msg.datapath
+        ofproto = dpath.ofproto
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        dst = eth.dst
+        src = eth.src
+
+        print("Packet Touched")
+
+        out = ofp_parser.OFPPacketOut(datapath=dp,in_port=msg.in_port,actions=actions)
+        dp.send_msg(out)
