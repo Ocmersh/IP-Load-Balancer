27a2b3884d3345fb690d304093b0beeebd1926f8
Bryce Hansen <ocmersh@gmail.com>
2019-03-27 02:06:01 -0600

    global vars

--- PythonApplication1/U0804551.py
+++ PythonApplication1/U0804551.py
@@ -9,10 +9,17 @@ from ryu.lib.of_config import *
 
 class IPLoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    current = []
+    next = []
+    initialMac = "00:00:00:00:00:01"
+    initialIP = "10.0.0.1"
+    virtualIP = "10.0.0.15"
 
     def __init__(self, *args, **kwargs):
         super(IPLoadBalancer, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        self.portMac = {}
+        self.current = self.initialIP
+        self.next = self.initialIP
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def currentPacket(self, ev):
@@ -24,13 +31,14 @@ class IPLoadBalancer(app_manager.RyuApp):
         sourcePort = msg.match['in_port']
         packetData = packet.Packet(msg.data)
         ethProt = packetData.get_protocol(ethernet.ethernet)
+
         dst = ethProt.dst
         src = ethProt.src
-        self.mac_to_port.setdefault(dpath.id, {})
-        self.mac_to_port[dpath.id][src] = sourcePort
+        self.portMac.setdefault(dpath.id, {})
+        self.portMac[dpath.id][src] = sourcePort
 
-        if dst in self.mac_to_port[dpath.id]:
-            out_port = self.mac_to_port[dpath.id][dst]
+        if dst in self.portMac[dpath.id]:
+            out_port = self.portMac[dpath.id][dst]
         else:
             out_port = opflow.OFPP_FLOOD
         actions = [opParse.OFPActionOutput(out_port)]
@@ -38,6 +46,6 @@ class IPLoadBalancer(app_manager.RyuApp):
         if msg.buffer_id == opflow.OFP_NO_BUFFER:
             data = msg.data
 
-        self.mac_to_port[dpath.id][src] = sourcePort
+        self.portMac[dpath.id][src] = sourcePort
         out = opParse.OFPPacketOut(datapath=dpath, buffer_id=msg.buffer_id,in_port=sourcePort, actions=actions, data=data)
         dpath.send_msg(out)
