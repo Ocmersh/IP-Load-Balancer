e97f1fe256240e1762e4060b71ed71d1fd1eb15d
Bryce Hansen <ocmersh@gmail.com>
2019-03-27 00:45:03 -0600

    test2

--- PythonApplication1/U0804551.py
+++ PythonApplication1/U0804551.py
@@ -2,35 +2,34 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls,CONFIG_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import arp
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import ipv6
+from ryu.lib.packet import packet,ethernet,arp,ipv4,ipv6
+from ryu.lib.of_config import *
+from ryu.contrib.oslo.config import *
 
-class L2Forwarding(app_manager.RyuApp):
+class IPLoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(L2Forwarding, self).__init__(*args, **kwargs)
+        super(IPLoadBalancer, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def curr_packet(self, ev):
+    def currentPacket(self, ev):
 
         msg = ev.msg
         dpath = msg.datapath
         opflow = dpath.ofproto
         opParse = dpath.ofproto_parser
-        in_port = msg.match['in_port']
+        sourcePort = msg.match['in_port']
         packetData = packet.Packet(msg.data)
         ethProt = packetData.get_protocol(ethernet.ethernet)
         dst = ethProt.dst
         src = ethProt.src
         self.mac_to_port.setdefault(dpath.id, {})
-        self.mac_to_port[dpath.id][src] = in_port
+        self.mac_to_port[dpath.id][src] = sourcePort
+
         if dst in self.mac_to_port[dpath.id]:
             out_port = self.mac_to_port[dpath.id][dst]
         else:
@@ -39,6 +38,7 @@ class L2Forwarding(app_manager.RyuApp):
         data = None
         if msg.buffer_id == opflow.OFP_NO_BUFFER:
             data = msg.data
-        self.mac_to_port[dpath.id][src] = in_port
-        out = opParse.OFPPacketOut(datapath=dpath, buffer_id=msg.buffer_id,in_port=in_port, actions=actions, data=data)
+
+        self.mac_to_port[dpath.id][src] = sourcePort
+        out = opParse.OFPPacketOut(datapath=dpath, buffer_id=msg.buffer_id,in_port=sourcePort, actions=actions, data=data)
         dpath.send_msg(out)
