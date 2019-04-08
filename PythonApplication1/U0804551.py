a914ab4e8e34a42b104d76d3f0ad6dd06e98e1e0
Bryce Hansen <ocmersh@gmail.com>
2019-04-08 00:03:29 -0600

    Restart

--- PythonApplication1/U0804551.py
+++ PythonApplication1/U0804551.py
@@ -9,71 +9,31 @@ from ryu.lib.of_config import *
 
 class IPLoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    switchIP = "10.0.0.10"
+    serverList = []
+    clientList = []
+    ip2mac = {}
+    ip2port = {}
+    currentHostIP = 0
 
     def __init__(self, *args, **kwargs):
         super(IPLoadBalancer, self).__init__(*args, **kwargs)
-        self.portMac = {}
-        self.backList = []
-        self.frontList = []
-        self.frontList.append({'ip':"10.0.0.1", 'mac':"00:00:00:00:00:01",'port':1})
-        self.frontList.append({'ip':"10.0.0.2", 'mac':"00:00:00:00:00:02",'port':2})
-        self.frontList.append({'ip':"10.0.0.3", 'mac':"00:00:00:00:00:03",'port':3})
-        self.frontList.append({'ip':"10.0.0.4", 'mac':"00:00:00:00:00:04",'port':4})
-        self.backList.append({'ip':"10.0.0.5", 'mac':"00:00:00:00:00:05",'port':5})
-        self.backList.append({'ip':"10.0.0.6", 'mac':"00:00:00:00:00:06",'port':6})
-        self.current = self.backList[0]['ip']
-        self.next = self.backList[0]['ip']
-        self.virtualIP = "10.0.0.15"
+        #add custom config to server list
+        self.serverList.append({'ip':"10.0.0.1",'10.0.0.1':1,'mac':"00:00:00:00:00:01",'port':1})
+        self.currentHostIP = self.serverList[currentHost]['ip']
 
 
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def currentPacket(self, ev):
-        messageIn = ev.msg
-        dpath = messageIn.datapath
-        opflow = dpath.ofproto
-        opParse = dpath.ofproto_parser
-        sourcePort = messageIn.match['in_port']
-        packetData = packet.Packet(messageIn.data)
-        ethProt = packetData.get_protocol(ethernet.ethernet)
-
-        dst = ethProt.dst
-        src = ethProt.src
-        self.portMac.setdefault(dpath.id, {})
-        self.portMac[dpath.id][src] = sourcePort
-
-        #self.addToTable()
+        inbound = ev.msg #
+        packetData = packet.Packet(inbound.data)
+        self.forwarding(inbound.datapath, packetData, packetData.get_protocol(ethernet.ethernet), inbound.datapath.ofproto_parser, inbound.datapath.ofproto, inbound.match['in_port'])
         
+        #respond
 
-        #arpProt = packetData.get_protocol(arp.arp)
-        #arpdst = arpProt.dst
-        #arpsrc = arpProt.src
-        #arpmacdest = ethProt.src
+        self.current = self.next
 
-        #loop through server list round robin
-        #setnext
-
-
-        #self.current = self.next
-
-        ##############
-        if dst in self.portMac[dpath.id]:
-            out_port = self.portMac[dpath.id][dst]
-        else:
-            out_port = opflow.OFPP_FLOOD
-        actions = [opParse.OFPActionOutput(out_port)]
-        data = None
-        if messageIn.buffer_id == opflow.OFP_NO_BUFFER:
-            data = messageIn.data
-            
-        self.portMac[dpath.id][src] = sourcePort
-        out = opParse.OFPPacketOut(datapath=dpath, buffer_id=messageIn.buffer_id,in_port=sourcePort, actions=actions, data=data)
-        dpath.send_msg(out)
-        ###############
-
-    def addToTable(dpath, packetData, ethProt, opParse, opflow, sourcePort):
-        x =1
-
-    def messageForward():
-        x =1
+    def forwarding(self, currentPath, packet, Eprotocol, parsedData, openflow, in_port):
+        x=1
 
