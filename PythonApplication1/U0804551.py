363bc77b91f16fcfc0e93cd11b7427ab8c95b321
Bryce Hansen <ocmersh@gmail.com>
2019-04-08 18:44:10 -0600

    Comments and bugs

--- PythonApplication1/U0804551.py
+++ PythonApplication1/U0804551.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Author: Bryce Hansen - U0804551
+#
+#This simple load balancer can handle up to a total of 99 total servers and clients.
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
@@ -9,11 +12,13 @@ from ryu.lib.packet import packet,ethernet,arp,ether_types
 from ryu import cfg
 
 class IPLoadBalancer(app_manager.RyuApp):
+    # The switches default IP as well as the number of front and back servers
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     switchIP = "10.0.0.10"
     front = 4
     back = 2
-    serverList = []
+    frontList = []
+    backList = []
     ip2mac = {}
     ip2port = {}
     currentHostIP = 0
@@ -21,10 +26,12 @@ class IPLoadBalancer(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(IPLoadBalancer, self).__init__(*args, **kwargs)
+        #Import and format config file
         CONF = cfg.CONF
         CONF.register_opts([cfg.IntOpt('front',default=4,help=('Number of front end clients')),cfg.IntOpt('back',default=2,help=('Number of back end servers')), cfg.StrOpt('switchIP',default='10.0.0.10',help=('IP address of the switch'))])
         self.switchIP = CONF.switchIP
 
+        # checks to see if the total of front + back servers is greater than 99, if so it will reduce them to under 99.
         if CONF.front+CONF.back < 99:
             self.front = CONF.front
             self.back = CONF.back
@@ -39,49 +46,56 @@ class IPLoadBalancer(app_manager.RyuApp):
             else:
                 self.back = CONF.back
 
+        #generates IP MAC and Port addresses to lists for the front and back servers. Also saves them to easily accessible sets.
         for initFront in range(1,self.front+1):
             if initFront < 10:
-                self.serverList.append(('10.0.0.{}'.format(initFront),'00:00:00:00:00:0{}'.format(initFront),initFront))
+                self.frontList.append(('10.0.0.{}'.format(initFront),'00:00:00:00:00:0{}'.format(initFront),initFront))
                 self.ip2mac['10.0.0.{}'.format(initFront)] = '00:00:00:00:00:0{}'.format(initFront)
             else:
-                self.serverList.append(('10.0.0.'.format(initFront),'00:00:00:00:00:{}'.format(initFront),initFront))
+                self.frontList.append(('10.0.0.'.format(initFront),'00:00:00:00:00:{}'.format(initFront),initFront))
                 self.ip2mac['10.0.0.{}'.format(initFront)] = '00:00:00:00:00:{}'.format(initFront)
 
             self.ip2port['10.0.0.{}'.format(initFront)] = initFront
 
-        for initBack in range(self.front+1,self.front+self.back+1):
+        for initBack in range(1,self.back+1):
             if initBack < 10:
-                self.serverList.append(('10.0.0.{}'.format(initBack),'00:00:00:00:00:0{}'.format(initBack),initBack))
+                self.backList.append(('10.0.0.{}'.format(initBack),'00:00:00:00:00:0{}'.format(initBack),initBack))
                 self.ip2mac['10.0.0.{}'.format(initBack)] = '00:00:00:00:00:0{}'.format(initBack)
             else:
-                self.serverList.append(('10.0.0.{}'.format(initBack),'00:00:00:00:00:{}'.format(initBack),initBack))
+                self.backList.append(('10.0.0.{}'.format(initBack),'00:00:00:00:00:{}'.format(initBack),initBack))
                 self.ip2mac['10.0.0.{}'.format(initBack)] = '00:00:00:00:00:{}'.format(initBack)
 
             self.ip2port['10.0.0.{}'.format(initBack)] = initBack
 
-        self.currentHostIP = self.serverList[0]
+        #current server is the first backend server
+        self.currentHostIP = self.backList[0]
         self.nextHostIP = 1
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def currentPacket(self, ev):
+        #get basic data from packet
         inbound = ev.msg #
         packetData = packet.Packet(inbound.data)
 
+        #send a response
         if packetData.get_protocol(ethernet.ethernet) == ether_types.ETH_TYPE_ARP:
             self.forwarding(inbound.datapath, packetData.get_protocol(ethernet.ethernet), packetData, inbound.datapath.ofproto, inbound.datapath.ofproto_parser, inbound.match['in_port'])
             self.returning(inbound.datapath, packetData.get_protocol(ethernet.ethernet), packetData, inbound.datapath.ofproto, inbound.datapath.ofproto_parser, inbound.match['in_port'])
 
+            #Get ARP info
             arpInbound = inbound.datapath.get_protocol(arp.arp)
             arpDestination = arpInbound.src_ip
             arpSource = arpInbound.dst_ip
             arpMac = packetData.src
 
-            for request in range (self.front+1,self.front+self.back+1):
+            #If the ARP request is from the back servers, set to return to host's IP
+            for request in range (1,self.back+1):
                 if request-(self.front+1) < self.back:
-                    if arpDestination == self.serverList[request]:
+                    if arpDestination == self.frontList[request]:
                         outBoundMac = self.ip2mac[arpSource]
+                #ELse choose MAC address from next server
                 else:
-                    outBoundMac = self.serverList[self.currentHostIP][1]
+                    outBoundMac = self.backList[self.currentHostIP][1]
 
                     self.nextHostIP += 1
                     if self.nextHostIP > self.back:
@@ -89,6 +103,7 @@ class IPLoadBalancer(app_manager.RyuApp):
 
                     break
 
+            #create new packet and send to decided IP
             outEthernet = ethernet.ethernet(arpMac, arpSource, ether_types.ETH_TYPE_ARP)
             outArp = arp.arp(1, 0x0800, 6, 4, 2, outBoundMac, arpSource, arpDestination, arpMac)
             outPacket = Packet()
@@ -102,23 +117,28 @@ class IPLoadBalancer(app_manager.RyuApp):
 
         return
 
+    #Forwards the ARP request to the next server
     def forwarding(self, packet, currentPath, Eprotocol, openflow, parsedData, in_port):
         inbound = packet.get_protocol(arp.arp).src_ip
 
-        for serverIp in range (self.front+1,self.front+self.back+1):
+        #if from back IP's, return
+        for serverIp in range (1,self.back+1):
             if serverIp-(self.front+1) > self.back:
                 break
             else:
-                if inbound == self.serverList[serverIp][0]:
+                if inbound == self.backList[serverIp][0]:
                     return
 
+        #parse and forward response
         parsing = parsedData.OFPMatch(in_port=in_port,ipv4_dst=self.switchIP,eth_type=0x0800)
         outbound = [parsedData.OFPActionSetField(ipv4_src=self.currentHostIP),parsedData.OFPActionOutput(self.ip2port[self.currentHostIP])]
         instructions = [parsedData.OFPInstructionActions(openflow.OFPIT_APPLY_ACTIONS, outbound)]
         outboundData = parsedData.OFPFlowMod(datapath=currentPath,priority=0,buffer_id=openflow.OFP_NO_BUFFER, match=parsing, instructions=instructions)
         currentPath.send_msg(outboundData)
-
+    
+    #A return response that reverses the forward response
     def returning(self, packet, currentPath, Eprotocol, openflow, parsedData, in_port):
+        #parse and return response
         inbound = packet.get_protocol(arp.arp).src_ip
         parsing = parsedData.OFPMatch(in_port=self.ip2port[self.currentHostIP],ipv4_src=self.currentHostIP,ipv4_dst=inbound,eth_type=0x0800)
         outbound = [parsedData.OFPActionSetField(ipv4_src=self.switchIP),parsedData.OFPActionOutput(in_port)]
